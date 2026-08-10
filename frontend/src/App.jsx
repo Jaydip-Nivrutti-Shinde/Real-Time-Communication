0e3dd082e904f01b7f7317ba2edc4a301d33062c
Jaydip Shinde <jns18jan@gmail.com>
2026-08-10 23:41:26 +0530

    Frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,17 @@ import {Routes, BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+    <h1>Hello</h1>
       <Router>
         <Routes>
           {/* <Route path="/home" element={}> </Route> */}
           <Route path="/" element={<Landing></Landing>}> </Route>
         </Routes>
       </Router>
-
     </>
   )
 }
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function Landing() {
   return (
-    <div>Landing</div>
+    <div className='landingPageContainer'>
+      <nav>
+        <div className='navHeader'><h2>Video Call</h2></div>
+        <div className='navList'>
+          <p>join as guest</p>
+          <p>register</p>
+          <div role='button'>
+            <p>Login</p>
+          </div>
+        </div>
+      </nav>
+      <div className="landingMainContainer">
+        <div>
+          <h1><span style={{color:"orange"}}>Connect</span> With you loved ones</h1>
+          <p>Cover a disctnace by apna video call</p>
+          <div role='button'>
+            <Link to="/home">Get Started</Link>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="Video Call" />
+        </div>
+      </div>
+    </div>
   )
 }
