aaad37640894b3a6868e2fd13d4c49596ae3ba08
Jaydip Shinde <jns18jan@gmail.com>
2026-08-15 23:34:03 +0530

    Updated videoMeet

--- frontend/src/pages/videoMeet.jsx
+++ frontend/src/pages/videoMeet.jsx
@@ -372,7 +372,7 @@ export default function VideoMeetComponent() {
         ctx.resume()
         return Object.assign(dst.stream.getAudioTracks()[0], { enabled: false })
     }
-    let black = ({ width = 640, height = 480 } = {}) => {
+    let black = ({ width = 640, height = 480 } = {}) =>  {
         let canvas = Object.assign(document.createElement("canvas"), { width, height })
         canvas.getContext('2d').fillRect(0, 0, width, height)
         let stream = canvas.captureStream()
